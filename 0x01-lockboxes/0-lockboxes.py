c37e1e218c983664f279fed32d484f6a0484255e
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2023-12-05 18:05:48 +0100

    [Feature] Adding a function to solved the lockboxes problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Method to determine if all boxes can be unlock"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all boxes can be opened
+       Arg:
+          boxes: list of lists
+       Return:
+          True or False
+    """
+    n = len(boxes)
+    opened = set()
+    opened.add(0)   # The first boxe is already opened
+
+    keys = [0]   # stack used to store keys or index of boxes not yet traversed
+
+    while keys:
+        current_box = keys.pop()
+        for key in boxes[current_box]:
+            if key not in opened:
+                opened.add(key)
+                keys.append(key)
+    return len(opened) == n
