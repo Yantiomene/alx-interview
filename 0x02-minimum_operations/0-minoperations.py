bb8d3cdad2fa9c38e1ab28ac5de2ec0928c0ca30
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2023-12-14 21:57:49 +0100

    [Feature] Function to compute the minimum number of operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations
+    Args:
+         n: int
+    Return:
+           an integer
+    """
+    if n == 1:
+        return 0
+    
+    divisor = 2
+    operations = 0
+
+    while n > 1:
+        while n % divisor == 0:
+            n //= divisor
+            operations += divisor
+
+        divisor += 1
+
+    return operations
