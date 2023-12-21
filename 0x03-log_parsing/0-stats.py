748e88e1ab862fb54ef12a3cdcf81f260c8c1dba
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2023-12-21 21:44:52 +0100

    [Feature] Script to read line in stdin and parse it to print stat

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Log Parsing"""
+import sys
+
+status_dic = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+total_size = 0
+count = 0
+try:
+    for line in sys.stdin:
+        line_content = line.split(" ")
+        if len(line_content) > 4:
+
+            file_size = line_content[-1]
+            status = line_content[-2]
+
+            if status in status_dic:
+                status_dic[status] += 1
+
+            total_size += int(file_size)
+            count += 1
+
+        if count == 10:
+            count = 0
+            print("File size: {}".format(total_size))
+            for k, v in sorted(status_dic.items()):
+                if v > 0:
+                    print("{}: {}".format(k, v))
+except Exception:
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for k, v in sorted(status_dic.items()):
+        if v > 0:
+            print("{}: {}".format(k, v))
