5c4ae39c0db7859f0a3e76bb8073168171d1b9e7
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-11 20:55:53 +0100

    Update README file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,16 +17,29 @@ def validUTF8(data):
 
     for byte in data:
         if nbytes == 0:
+            if byte >> 7 == 0:
+                continue
+            
             while byte & (1 << (7 - nbytes)):
                 nbytes += 1
 
             if nbytes == 1 or nbytes > 4:
+                print(f"byte: {byte} \nnbytes: {nbytes}")
                 return False
             if nbytes == 0:
                 continue
         else:
             if not(byte & BIT7 and not(byte & BIT6)):
+                print(f"byte: {byte} \nnbytes: {nbytes}")
                 return False
             nbytes -= 1
+            if nbytes > 0:
+                continue
 
+        if nbytes == 2 and byte < (1 << 7 + 1 << 6):
+            return False
+        elif nbytes == 3 and byte < (1 << 7 + 1 << 6 + 1 << 13):
+            return False
+
+    print(f"byte: {byte} \nnbytes: {nbytes}")
     return nbytes == 0
