608e3c335ba9cbcf75304f97ce2a92987efc52b5
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-01-18 21:59:38 +0100

    [Fix] Fixing the log order of people name

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,17 +2,25 @@
 
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+function getName (characterList, i) {
+  if (i === characterList.length) {
+    return;
+  }
+  request(characterList[i], function (err, response, body) {
+    if (err === null) {
+      const people = JSON.parse(body);
+      console.log(people.name);
+      getName(characterList, i + 1);
+    }
+  });
+}
+
 request(url, function (err, response, body) {
   if (err === null) {
     const film = JSON.parse(body);
     const characters = film.characters;
-    for (const charac of characters) {
-      request(charac, function (err, response, body) {
-        if (err === null) {
-          const people = JSON.parse(body);
-          console.log(people.name);
-        }
-      });
-    }
+
+    getName(characters, 0);
   }
 });
